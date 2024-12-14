4494e0e9ab216efdc889bb47ac5d69ac9ed9d3bb
Gaurav Mishra <gauravmishra92812@gmail.com>
2024-12-14 17:48:53 +0530

    email validator change

--- include/footer.php
+++ include/footer.php
@@ -3,8 +3,9 @@
         <div class="row">
             <div class="col-md-3 col-sm-6">
                 <div class="footer-widget about-widget">
-                    <a class="footer-logo"><img src="fine/bglogo.png" alt="Awesome Image"/></a>
-                    <p> Near Lalbag Ground Main Road,<br>Indravati Colony, Ward - 8, Lalbag,<br>Amaguda, Jagdalpur (CG) 494001</p>
+                    <a class="footer-logo"><img src="fine/bglogo.png" alt="Awesome Image" /></a>
+                    <p> Near Lalbag Ground Main Road,<br>Indravati Colony, Ward - 8, Lalbag,<br>Amaguda, Jagdalpur (CG)
+                        494001</p>
                     <p>Email: sentinexneedindia@gmail.com</p>
                     <p>Phone: +91 93047 69195</p>
                 </div><!-- /.footer-widget about-widget -->
@@ -16,27 +17,27 @@
                         <h3>Browse</h3>
                     </div><!-- /.title -->
                     <ul class="links-list">
-                      <li>
-                         <a href="contact.php#contact-form">
-                            <i class="fa fa-angle-right"></i> Contact Us
-                         </a>
-                      </li>
-                      <li>
-                         <a href="blog-grid.php">
-                            <i class="fa fa-angle-right"></i> Our Blogs
-                         </a>
-                      </li>
-                      <li>
-                         <a href="index.php#services">
-                            <i class="fa fa-angle-right"></i> services
-                         </a>
-                      </li>
-                      <li>
-                         <a href="about.php">
-                            <i class="fa fa-angle-right"></i> About Us
-                         </a>
-                      </li>
-                        
+                        <li>
+                            <a href="contact.php#contact-form">
+                                <i class="fa fa-angle-right"></i> Contact Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="blog-grid.php">
+                                <i class="fa fa-angle-right"></i> Our Blogs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="index.php#services">
+                                <i class="fa fa-angle-right"></i> services
+                            </a>
+                        </li>
+                        <li>
+                            <a href="about.php">
+                                <i class="fa fa-angle-right"></i> About Us
+                            </a>
+                        </li>
+
                     </ul><!-- /.links-list -->
                 </div><!-- /.footer-widget links-widget -->
             </div><!-- /.col-md-2 -->
@@ -62,9 +63,13 @@
                     </div><!-- /.title -->
                     <form action="footerForm.php" method="post" class="footer-contact-form">
                         <input type="text" placeholder="Name*" name="name" required />
-                        <input type="text" placeholder="Contact No.*" name="contact" required />
-                        <input type="text" placeholder="Email*" name="email" required />
-                        
+                        <input type="text" placeholder="Contact No.*" name="contact" required
+                            pattern="^\+?[1-9]\d{1,14}$"
+                            title="Please enter a valid phone number. It should start with a '+' followed by the country code and then the number, or just the number without a '+'." />
+                        <input type="email" id="email" placeholder="Email*" name="email" required
+                            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                            title="Please enter a valid email address." />
+
                         <button class="thm-btn" name="submit_form" type="submit">Send</button>
                     </form><!-- /.contact-form -->
                 </div><!-- /.footer-widget links-widget -->
@@ -75,7 +80,7 @@
 
 <div class="footer-bottom text-center">
     <div class="container">
-        <p>&copy;  Copyrights 2024. Sentinex Need India Organization All Rights Reserved</p>
+        <p>&copy; Copyrights 2024. Sentinex Need India Organization All Rights Reserved</p>
     </div><!-- /.container -->
 </div><!-- /.footer-bottom -->
 
@@ -83,12 +88,14 @@
 <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-long-arrow-up"></span></div>
 
 <!-- Modal -->
-<div class="modal fade search-form-modal" id="search-form-modal" tabindex="-1" role="dialog" aria-labelledby="search-form-modal">
+<div class="modal fade search-form-modal" id="search-form-modal" tabindex="-1" role="dialog"
+    aria-labelledby="search-form-modal">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-body">
                 <form action="#" class="popup-search-form">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
                     <input type="text" placeholder="Enter Your Text Here...." />
                     <button type="submit" class="fa fa-search"></button>
                 </form>
@@ -124,4 +131,5 @@
 <script src="js/custom.js"></script>
 
 </body>
+
 </html>
